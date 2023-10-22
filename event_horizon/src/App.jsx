af9d34541d6d80da597510285e10b5b4009796d6
Kartikey Sachdeva <kartisa@Kartikeys-MacBook-Pro.local>
2023-10-21 22:14:35 -0400

    login/signup page frontend implemented

--- event_horizon/src/App.jsx
+++ event_horizon/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./components/pages/Home";
 import Events from "./components/pages/Events";
 import Clubs from "./components/pages/Clubs";
 import Login from "./components/pages/Login";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/events" exact Component={Events} />
           <Route path="/clubs" exact Component={Clubs} />
           <Route path="/login" exact Component={Login} />
+          <Route path="/signup" exact Component={SignUp} />
         </Routes>
       </Router>
     </>
