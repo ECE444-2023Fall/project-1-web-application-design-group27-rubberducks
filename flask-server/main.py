4f791307a036e8da5075c8732f124eea39e584c6
Danny Ahmad <43858798+daniql@users.noreply.github.com>
2023-10-25 03:18:14 -0400

    migrated to postgres and created a better api

--- flask-server/main.py
+++ flask-server/main.py
@@ -1,137 +1,159 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Account, Event, Host
 from exts import db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db.init_app(app)
-api=Api(app, doc='/docs')
+api = Api(app, doc="/docs")
 
-event_model = api.model('Event', {
-    'eid': fields.Integer,
-    'name': fields.String,
-    'description': fields.String,
-    'location': fields.String,
-    'date': fields.String,
-    'time': fields.String,
-    'host': fields.String
-})
+event_model = api.model(
+    "Event",
+    {
+        "eid": fields.Integer,
+        "name": fields.String,
+        "description": fields.String,
+        "location": fields.String,
+        "date": fields.String,
+        "capacity": fields.Integer,
+        "attendees": fields.List(fields.Integer),
+        "owner": fields.String,
+    },
+)
 
-account_model = api.model('Account', {
-    'aid': fields.Integer,
-    'name': fields.String,
-    'email': fields.String,
-    'password': fields.String,
-    'host': fields.String
-})
+account_model = api.model(
+    "Account",
+    {
+        "uid": fields.Integer,
+        "name": fields.String,
+        "email": fields.String,
+        "password": fields.String,
+        "events": fields.List(fields.Integer),
+        "fav_events": fields.List(fields.Integer),
+        "orgs": fields.List(fields.Integer),
+    },
+)
 
-host_model = api.model('Host', {
-    'hid': fields.Integer,
-    'name': fields.String,
-    'email': fields.String,
-    'bio': fields.String,
-    'events': fields.String,
-    'owner': fields.String
-})
-    
-@api.route('/events')
+host_model = api.model(
+    "Host",
+    {
+        "hid": fields.Integer,
+        "name": fields.String,
+        "email": fields.String,
+        "bio": fields.String,
+        "events": fields.List(fields.Integer),
+        "owner": fields.Integer,
+    },
+)
+
+
+@api.route("/events")
 class Events(Resource):
-    @api.marshal_with(event_model)
+    @api.marshal_list_with(event_model)
     def get(self):
         return Event.query.all()
-    
+
     @api.expect(event_model)
     @api.marshal_with(event_model)
     def post(self):
         event = Event(**api.payload)
         event.save()
-        return event,201
-    
-@api.route('/events/<int:eid>')
+        return event, 201
+
+
+@api.route("/events/<int:eid>")
 class EventById(Resource):
     @api.marshal_with(event_model)
     def get(self, eid):
-        return Event.query.filter_by(eid=eid).first()
-    
+        event = Event.query.get_or_404(eid)
+        return event
+
     @api.expect(event_model)
     @api.marshal_with(event_model)
     def put(self, eid):
-        event = Event.query.filter_by(eid=eid).first()
-        event.update_name(**api.payload)
+        event = Event.query.get_or_404(eid)
+        event.update(**api.payload)
         return event
-    
-    def delete(self, eid):
-        event = Event.query.filter_by(eid=eid).first()
-        event.delete()
-        return {'message': 'event deleted'}
 
-@api.route('/accounts')
+    def delete(self, eid):
+        event = Event.query.get_or_404(eid)
+        event.delete()
+        return {"message": "event deleted"}
+
+
+@api.route("/accounts")
 class Accounts(Resource):
-    @api.marshal_with(account_model)
+    @api.marshal_list_with(account_model)
     def get(self):
         return Account.query.all()
-    
+
     @api.expect(account_model)
     @api.marshal_with(account_model)
     def post(self):
         account = Account(**api.payload)
         account.save()
-        return account,201
+        return account, 201
 
-@api.route('/accounts/<int:uid>')
+
+@api.route("/accounts/<int:uid>")
 class AccountById(Resource):
     @api.marshal_with(account_model)
     def get(self, uid):
-        return Account.query.filter_by(uid=uid).first()
-    
+        return Account.query.get_or_404(uid)
+
     @api.expect(account_model)
     @api.marshal_with(account_model)
     def put(self, uid):
-        account = Account.query.filter_by(uid=uid).first()
-        account.update_name(**api.payload)
+        account = Account.query.get_or_404(uid)
+        account.update(**api.payload)
         return account
-    
-    def delete(self, uid):
-        account = Account.query.filter_by(uid=uid).first()
-        account.delete()
-        return {'message': 'account deleted'}
 
-@api.route('/hosts')
+    def delete(self, uid):
+        account = Account.query.get_or_404(uid)
+        account.delete()
+        return {"message": "account deleted"}
+
+
+@api.route("/hosts")
 class Hosts(Resource):
-    @api.marshal_with(host_model)
+    @api.marshal_list_with(host_model)
     def get(self):
         return Host.query.all()
-    
+
     @api.expect(host_model)
     @api.marshal_with(host_model)
     def post(self):
         host = Host(**api.payload)
         host.save()
-        return host,201
+        return host, 201
 
-@api.route('/hosts/<int:hid>')
+
+@api.route("/hosts/<int:hid>")
 class HostById(Resource):
     @api.marshal_with(host_model)
     def get(self, hid):
-        return Host.query.filter_by(hid=hid).first()
-    
+        return Host.query.get_or_404(hid)
+
     @api.expect(host_model)
     @api.marshal_with(host_model)
     def put(self, hid):
-        host = Host.query.filter_by(hid=hid).first()
-        host.update_name(**api.payload)
+        host = Host.query.get_or_404(hid)
+        host.update(**api.payload)
         return host
-    
+
     def delete(self, hid):
-        host = Host.query.filter_by(hid=hid).first()
+        host = Host.query.get_or_404(hid)
         host.delete()
-        return {'message': 'host deleted'}
-    
+        return {"message": "host deleted"}
+
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Account': Account, 'Host': Host, 'Event': Event}
-    
-if __name__ == '__main__':
+    return {"db": db, "Account": Account, "Host": Host, "Event": Event}
+
+
+if __name__ == "__main__":
     app.run(port=8000)
--- flask-server/models.py
+++ flask-server/models.py
@@ -1,25 +1,39 @@
 from exts import db
+from sqlalchemy.dialects.postgresql import JSON
 
-'''
+"""
 class Account:
     uid: int primary key
     name: str
     email: str unique
     password: str
     events: list of Event
-'''
+    fav_events: list of Event
+    orgs: list of Host
+"""
+
 
 class Account(db.Model):
-    __tablename__ = 'account'
+    __tablename__ = "account"
     uid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
-    events = db.Column(db.Integer, nullable=False)
+    events = db.Column(db.ARRAY(db.Integer))
+    fav_events = db.Column(db.ARRAY(db.Integer))
+    orgs = db.relationship("Host", backref="account")
+
+    def __init__(self, name, email, password, events, fav_events, orgs):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.events = events
+        self.fav_events = fav_events
+        self.orgs = orgs
 
     def __repr__(self):
-        return f'<Account {self.uid} {self.name} {self.email} {self.password}>'
-    
+        return f"<Account {self.uid} {self.name} {self.email} {self.password} {self.events} {self.fav_events} {self.orgs}>"
+
     def save(self):
         db.session.add(self)
         db.session.commit()
@@ -28,13 +42,17 @@ class Account(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def update_name(self, name, email, password):
+    def update(self, name, email, password, events, fav_events, orgs):
         self.name = name
         self.email = email
         self.password = password
+        self.events = events
+        self.fav_events = fav_events
+        self.orgs = orgs
         db.session.commit()
 
-'''
+
+"""
 class Host:
     hid: int primary key
     name: str
@@ -42,20 +60,28 @@ class Host:
     bio: str
     events: list of Event
     owner: Account
-'''
+"""
+
 
 class Host(db.Model):
-    __tablename__ = 'host'
+    __tablename__ = "host"
     hid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     bio = db.Column(db.String(50), nullable=False)
-    events = db.Column(db.Integer, nullable=False)
-    owner = db.Column(db.Integer, nullable=False)
+    events = db.Column(db.ARRAY(db.Integer))
+    owner = db.Column(db.Integer, db.ForeignKey("account.uid"), nullable=False)
+
+    def __init__(self, name, email, bio, events, owner):
+        self.name = name
+        self.email = email
+        self.bio = bio
+        self.events = events
+        self.owner = owner
 
     def __repr__(self):
-        return f'<Host {self.hid} {self.name} {self.email} {self.bio} {self.events} {self.owner}>'
-    
+        return f"<Host {self.hid} {self.name} {self.email} {self.bio} {self.events} {self.owner}>"
+
     def save(self):
         db.session.add(self)
         db.session.commit()
@@ -64,7 +90,7 @@ class Host(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def update_name(self, name, email, bio, events, owner):
+    def update(self, name, email, bio, events, owner):
         self.name = name
         self.email = email
         self.bio = bio
@@ -72,7 +98,8 @@ class Host(db.Model):
         self.owner = owner
         db.session.commit()
 
-'''
+
+"""
 class Event:
     eid: int primary key
     name: str
@@ -81,21 +108,30 @@ class Event:
     capacity: int
     attendees: list of Account
     owner: Host
-'''
+"""
+
 
 class Event(db.Model):
-    __tablename__ = 'event'
+    __tablename__ = "event"
     eid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     location = db.Column(db.String(50), nullable=False)
     date = db.Column(db.Date, nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
-    attendees = db.Column(db.Integer, nullable=False)
-    owner = db.Column(db.Integer, nullable=False)
+    attendees = db.Column(db.ARRAY(db.Integer))
+    owner = db.Column(db.Integer, db.ForeignKey("host.hid"), nullable=False)
+
+    def __init__(self, name, location, date, capacity, attendees, owner):
+        self.name = name
+        self.location = location
+        self.date = date
+        self.capacity = capacity
+        self.attendees = attendees
+        self.owner = owner
 
     def __repr__(self):
-        return f'<Event {self.eid} {self.name} {self.location} {self.date} {self.capacity} {self.attendees} {self.owner}>'
-    
+        return f"<Event {self.eid} {self.name} {self.location} {self.date} {self.capacity} {self.attendees} {self.owner}>"
+
     def save(self):
         db.session.add(self)
         db.session.commit()
@@ -104,7 +140,7 @@ class Event(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def update_name(self, name, location, date, capacity, attendees, owner):
+    def update(self, name, location, date, capacity, attendees, owner):
         self.name = name
         self.location = location
         self.date = date
