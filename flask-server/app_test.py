77ab2f8e751c0dbdffbd03b10fbd143fa39ab955
Danny Ahmad <43858798+daniql@users.noreply.github.com>
2023-10-25 22:59:58 -0400

    more tests

--- flask-server/app_test.py
+++ flask-server/app_test.py
@@ -20,13 +20,32 @@ class TestAccounts(BaseTest):
         response=self.client.get("/accounts/")
         self.assertEqual(response.status_code, 200)
 
+    def test_post_account(self):
+        response=self.client.post("/accounts/", json={
+            "name": "Test",
+            "email": "test@utoronto.ca",
+            "password": "test",
+            "events": [],
+            "fav_events": [],
+            "orgs": [],
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json["name"], "Test")
+        self.assertEqual(response.json["email"], "test@utoronto.ca")
+        self.assertEqual(response.json["password"], "test")
+        self.assertEqual(response.json["events"], [])
+        self.assertEqual(response.json["fav_events"], [])
+        self.assertEqual(response.json["orgs"], [])
+
 class TestEvents(BaseTest):          
     def test_get_all_events(self):
         response=self.client.get("/events/")
         self.assertEqual(response.status_code, 200)
 
 class TestHosts(BaseTest):
-    pass
+    def test_get_all_hosts(self):
+        response=self.client.get("/hosts/")
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
