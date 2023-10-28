aa46ea0737a5bab091cdc14f9ebecd47c11b23c4
CHRISTIAN MCINTOSH_CLARKE <clarkechristian5@gmail.com>
2023-10-28 13:44:56 -0400

    added the test for getting host by name

--- flask-server/tests/test_app/test_hosts.py
+++ flask-server/tests/test_app/test_hosts.py
@@ -8,3 +8,8 @@ def test_get_host_by_hid(client, host, add_host_to_db):#Chrstian McIntosh Clarke
         response = client.get("/hosts/1")
         assert response.status_code == 200
         assert response.json == host
+
+def test_get_host_by_name(client, host, add_host_to_db):#Chrstian McIntosh Clarke
+        response = client.get("/hosts/Test Host 1")
+        assert response.status_code == 200
+        assert response.json == host
