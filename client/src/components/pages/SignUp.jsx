6f49ec10adbbf62d59d7b93f6a99d5db2cf0fdd2
Kartikey Sachdeva <kartisa@Kartikeys-MacBook-Pro.local>
2023-10-29 19:55:44 -0400

    updated backend configuration with login/signup page with user success/error deisplaying on client side with some other minor UI fixes

--- client/src/components/pages/SignUp.jsx
+++ client/src/components/pages/SignUp.jsx
@@ -2,79 +2,142 @@ import React, { useState } from "react";
 import "../../Login.css";
 
 function SignUp() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [remember, setRemember] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [confirmEmail, setConfirmEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState(""); // New state for confirming password
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [signupSuccess, setSignupSuccess] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Email:", email, "Password:", password, "Remember me:", remember);
-  };
+    const submitSignUpForm = (e) => {
+        e.preventDefault();
 
-  return (
-    <div className="login-container">
-      <div className="login-card">
-        <h2>Welcome!</h2>
-        <form onSubmit={handleSubmit}>
-        <div className="input-group">
-            <i className="fas fa-user"></i>
-            <input
-              type="email"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <i className="fas fa-envelope"></i>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <i className="fas fa-lock"></i>
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <span 
-              className="toggle-password" 
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? <i className="fas fa-eye-slash"></i> : <i className="fas fa-eye"></i>}
-            </span>
-          </div>
-          <div className="additional-options">
-            <label>
-              <input type="checkbox" checked={remember} onChange={() => setRemember(!remember)} />
-              Remember me
-            </label>
-            {/* <a href="#!" className="forgot-password">Forgot Password?</a> */}
-          </div>
-          <p className="error-message">Error message placeholder</p>
-          <button type="submit">Sign Up</button>
-          
-          
-          <div className="signup-option">
-            <span>or</span>
-            <a href="/login">Log In</a>
-          </div>
+        if (email !== confirmEmail) {
+            setErrorMessage("Emails do not match.");
+            return;
+        }
 
+        if (password !== confirmPassword) {
+            setErrorMessage("Passwords do not match.");
+            return;
+        }
 
-        </form>
-      </div>
-    </div>
-  );
+        fetch("/api/auth/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+            }),
+        })
+        .then((res) => {
+            if (res.status === 409) {
+                throw new Error("Email already in use");
+            } else if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            setName("");
+            setEmail("");
+            setConfirmEmail("");
+            setPassword("");
+            setConfirmPassword(""); // Reset confirmed password
+            setErrorMessage("");
+            setSignupSuccess(true);
+            // Maybe navigate to login or dashboard page after successful signup
+        })
+        .catch((err) => {
+            console.log("error:", err);
+            setErrorMessage(err.message);
+        });
+    };
+
+    return (
+        <div className="login-container">
+            <div className="login-card">
+                <h2>Create Account</h2>
+                <form onSubmit={submitSignUpForm}>
+                    <div className="input-group">
+                        <i className="fas fa-user"></i>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <i className="fas fa-envelope"></i>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <i className="fas fa-envelope"></i>
+                        <input
+                            type="email"
+                            placeholder="Confirm Email"
+                            value={confirmEmail}
+                            onChange={(e) => setConfirmEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <i className="fas fa-lock"></i>
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <span 
+                            className="toggle-password" 
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? <i className="fas fa-eye-slash"></i> : <i className="fas fa-eye"></i>}
+                        </span>
+                    </div>
+                    <div className="input-group">
+                        <i className="fas fa-lock"></i>
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Confirm Password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="additional-options">
+                        <label>
+                            <input type="checkbox" />
+                            Remember me
+                        </label>
+                    </div>
+                    {errorMessage && <div className="error-message">{errorMessage}</div>}
+                    {signupSuccess && <p className="success-message" style={{textAlign: 'center'}}>Signup successful!</p>}
+                    <button type="submit">Sign Up</button>
+                    <div className="signup-option">
+                        <span>or</span>
+                        <a href="/login">Log In</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default SignUp;
