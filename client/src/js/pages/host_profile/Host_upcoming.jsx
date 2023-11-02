ce80c131c1f501b111bd9680455eab550011a508
CHRISTIAN MCINTOSH_CLARKE <clarkechristian5@gmail.com>
2023-11-01 23:46:28 -0400

    upcoming/previous events pages by hid aswell

--- client/src/js/pages/host_profile/Host_upcoming.jsx
+++ client/src/js/pages/host_profile/Host_upcoming.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../../css/pages/host_profile/Host_upcoming.css";
 import Cards from "../../components/Cards";
 
 export default function Host_upcoming({hid}) {
+  const [hostname, setHostName] = useState("");
+  const [email, setEmail] = useState("");
+  const [bio, setBio] = useState("");
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch(`/api/hosts/${hid}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setHostName(data.name);
+        setEmail(data.email);
+        setBio(data.bio);
+        setEvents(data.events);
+      });
+  }, [hid]);
   return (
     <>
       <div className="sidebar">
-        <img src="../../../images/placeholder.png" alt="Profile Picture" />
-        <div className="host--name">host</div>
-        <div className="host--email">host@example.com</div>
+        <img src="../../../../images/placeholder.png" alt="Profile Picture" />
+        <div className="host--name">{hostname}</div>
+        <div className="host--email">{email}</div>
         <div className="host--bio">
           <h2 className="sidebar--heading">Bio</h2>
           <p className="sidebar--paragraph">
-            This is my bio. I am a host and I love to organize events.
+            {bio}
           </p>
         </div>
         <div className="host--tags">
@@ -24,7 +39,7 @@ export default function Host_upcoming({hid}) {
           <div className="card--header">
             <h2 className="card--heading">Upcoming Events</h2>
             <span className="card--see-all small">
-              <a href="/host_profile">Return to Profile</a>
+              <a href={`/host_profile/${hid}`}>Return to Profile</a>
             </span>
           </div>
           <Cards />
