9dbf7a151debbf18c22065d928469cc9ede75214
Rebecca L <rebeccalashleybb@gmail.com>
2023-11-02 20:38:48 -0400

    working on connecting backend

---
+++ client/src/js/pages/EventDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import EventDetails from "../components/EventDetails";
+
+function Events() {
+  // return <EventDetails/>;
+  return <div>Events</div>
+}
+
+export default Events;
