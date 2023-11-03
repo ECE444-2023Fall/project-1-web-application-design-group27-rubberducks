aa23b18b0e6970bb284c8c38b2ed71a7c708e0dc
Rebecca L <rebeccalashleybb@gmail.com>
2023-11-03 16:26:30 -0400

    working on connecting backend

--- client/src/js/pages/EventDetails.jsx
+++ client/src/js/pages/EventDetails.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+//import { useNavigate } from "react-router-dom";
 import EventDetails from "../components/EventDetails";
 
-function Events() {
-  // return <EventDetails/>;
-  return <div>Events</div>
+function EventDetails(eid) {
+  const [event, setEvent] = useState("");
+
+  useEffect(() => {
+
+    fetch("/api/events/${eid}")
+      .then((response) => response.json())
+      .then((data) => {
+        const jsonString = JSON.stringify(data, null, 2);
+        setEvent(jsonString);
+      })
+      .catch((error) => console.error("Failed to fetch event", error));
+
+  }, [eid]);
+
+
+  return (
+    <div>
+        {events.map(event => (
+          <EventDetails key={eid} event={event}/>
+        ))}
+    </div>
+  );
 }
 
-export default Events;
+export default EventDetails;
