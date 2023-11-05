33a6f8dce191b117e4523d47aa4e5f9f0f2c8ba1
Ruoyi <ruoyi.xie@mail.utoronto.ca>
2023-11-05 11:58:59 -0500

    Remove tag/location/data

--- client/src/js/components/UserCards.jsx
+++ client/src/js/components/UserCards.jsx
@@ -12,9 +12,9 @@ function UserCards({org,onCardClick}) {
             key={org.hid}
             src={org.image || "images/placeholder.png"} // assuming 'org' has an 'image' property
             text={org.name} // assuming 'org' has a 'name' property
-            label={org.category || "placeholder"} // assuming 'org' has a 'category' property
-            date={org.date || "placeholder date"} // assuming 'org' has a 'date' property
-            location={org.location || "placeholder location"} // assuming 'org' has a 'location' property
+            // label={org.category || "placeholder"} // assuming 'org' has a 'category' property
+            // date={org.date || "placeholder date"} // assuming 'org' has a 'date' property
+            // location={org.location || "placeholder location"} // assuming 'org' has a 'location' property
             path={`/host_profile/${org.hid}`}
             onClick={() => onCardClick(org)}
             />
