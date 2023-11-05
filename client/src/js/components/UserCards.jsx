80bfa92529b4e94820676e412ad1d55f44cdd14d
Ruoyi <ruoyi.xie@mail.utoronto.ca>
2023-11-05 11:18:30 -0500

    UserCards

---
+++ client/src/js/components/UserCards.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import CardItem from "./CardItem";
+import "../../css/components/Cards.css";
+
+function UserCards({org,onCardClick}) {
+  return (
+    <div className="cards">
+      <div className="cards--container">
+        <div className="cards--wrapper">
+          <ul className="cards--items">     
+            <CardItem
+            // src="images/placeholder.png"
+            // text="placeholder title"
+            // label="placeholder"
+            // date="placeholder date"
+            // location="placeholder location"
+            // path="/events"
+            key={org.id}
+            src={org.image || "images/placeholder.png"} // assuming 'org' has an 'image' property
+            text={org.name || "placeholder title"} // assuming 'org' has a 'name' property
+            label={org.category || "placeholder"} // assuming 'org' has a 'category' property
+            date={org.date || "placeholder date"} // assuming 'org' has a 'date' property
+            location={org.location || "placeholder location"} // assuming 'org' has a 'location' property
+            path={`/host_profile/${org}`}
+            onClick={() => onCardClick(org)}
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserCards;
