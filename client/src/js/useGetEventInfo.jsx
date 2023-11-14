d6b6d68056b5a03a25bbaeb73857cdb94f3ea6e5
Rebecca L <rebeccalashleybb@gmail.com>
2023-11-13 21:16:36 -0500

    add eventdetails changes from main

--- client/src/js/useGetEventInfo.jsx
+++ client/src/js/useGetEventInfo.jsx
@@ -2,25 +2,83 @@ import { useState, useEffect, useCallback } from "react";
 
 export const useGetEventInfo = (eventId) => {
   const [loading, setLoading] = useState(true);
+  const [hostInfo, setHostInfo] = useState({});
   const [eventInfo, setEventInfo] = useState({});
-
-  const loadEventInfo = useCallback(async () => {
-    const response = await fetch("/api/events/" + eventId);
-
-    if (response.status === 404) {
-      window.location.href = "/404";
-    } else if (response.ok) {
-      const data = await response.json();
-      setEventInfo(data);
-      setLoading(false);
-    } else {
-      console.log(response.status);
-    }
-  }, []);
+  const [userInfo, setUserInfo] = useState({});
+  const [loadingHostInfo, setLoadingHostInfo] = useState(true);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [ownerLoggedIn, setOwnerLoggedIn] = useState(false);
+  const [isAlreadyRegistered, setIsAlreadyRegistered] = useState(false);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const id = user ? user.id : null && setUserLoggedIn(false);
 
   useEffect(() => {
-    loadEventInfo();
-  }, [loadEventInfo]);
+    setLoading(true);
 
-  return { eventInfo, loading };
+    // fetch event information for given eventId
+    fetch("/api/events/" + eventId) 
+      .then((res) => {
+        if (!res.ok) {
+          window.location.href = "/404";
+        }
+        return res.json();
+      })
+      .then((eventData) => {
+        setEventInfo(eventData);
+        // fetch host info for host of given event
+        fetch("/api/hosts/" + eventData.owner)
+          .then((res) => {
+            if (!res.ok) {
+              window.location.href = "/404";
+            }
+            return res.json();
+          })
+          .then((hostData) => {
+            setHostInfo(hostData);
+            setLoadingHostInfo(false);
+          });
+
+      });
+  }, [eventId]);
+
+  // if there is a user logged in, fetch the user info
+  const loadUserInfo = useCallback(async () => {
+    if (id) {
+      fetch("/api/accounts/" + id)
+        .then((res) => res.json())
+        .then((data) => {
+          setUserInfo(data);
+          setUserLoggedIn(true);
+
+          if (!loadingHostInfo) {
+
+            // check if the user logged in is the owner of the event
+            if (hostInfo.owner === data.uid) {
+              setOwnerLoggedIn(true);
+            } else {
+              setOwnerLoggedIn(false);
+            }
+
+            // check if the user is already registered for the event
+            if (eventInfo.attendees.includes(data.uid)) {
+              setIsAlreadyRegistered(true);
+            } else {
+              setIsAlreadyRegistered(false);
+            }  
+
+            setLoading(false);
+          }
+        });
+    } else {
+      setOwnerLoggedIn(false);
+      setLoading(false);
+      setUserLoggedIn(false);
+    }
+  }, [id, hostInfo, loadingHostInfo]);
+
+  useEffect(() => {
+    loadUserInfo();
+  }, [loadUserInfo, loadingHostInfo]);
+
+  return { hostInfo, eventInfo, userInfo, userLoggedIn, ownerLoggedIn, isAlreadyRegistered, loading };
 };
